e266d79a8457412dce240f87c72206abf5d58cf6
brokengdnb <brokengdnb@gmail.com>
2020-10-04 02:42:25 +0200

    0.1.1 baseurl fix

--- docs/sw.js
+++ docs/sw.js
@@ -4,8 +4,8 @@ self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/pixyll.css?202010040232",
-        "/"
+        "/blog/css/pixyll.css?202010040241",
+        "/blog/"
       ]);
     })
   );
