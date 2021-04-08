8b70e439ba764a3f96557d33c34b3f4492713d0c
brokengdnb <brokengdnb@gmail.com>
2021-04-08 02:34:14 +0200

    0.3.1 WD image fix

--- docs/sw.js
+++ docs/sw.js
@@ -4,7 +4,7 @@ self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/pixyll.css?202104080231",
+        "/css/pixyll.css?202104080234",
         "/"
       ]);
     })
