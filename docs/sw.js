9c89650e8907fe932bd93d60d46bb1abba0c999e
brokengdnb <brokengdnb@gmail.com>
2021-10-31 11:07:31 +0100

    0.3.5 ffmpeg

--- docs/sw.js
+++ docs/sw.js
@@ -4,7 +4,7 @@ self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/pixyll.css?202104080324",
+        "/css/pixyll.css?202104080545",
         "/"
       ]);
     })
